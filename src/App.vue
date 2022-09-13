47e6f3a44c2f19e67d25b981e4fec52ec18e6663
Diego Zacarias <dzv3@me.com>
2022-09-13 01:20:04 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div class="my-8">
+      <QueryInput @input="onInputQuery" />
+    </div>
+
+    <div class="my-8">
+
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import QueryInput from "./components/QueryInput.vue";
+import { ref } from 'vue';
+
+const query = ref('');
+const onInputQuery = (value: string) => {
+  query.value = value;
+}
+</script>
+
+<style scoped>
+
+</style>
